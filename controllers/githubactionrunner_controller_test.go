e59be1c784cdbf108806ca5507228218fbb25e4a
David Karlsen <david@davidkarlsen.com>
2020-07-18 21:39:25 +0200

    migrate to kubebuilder setup

---
+++ controllers/githubactionrunner_controller_test.go
@@ -0,0 +1,113 @@
+package controllers
+
+import (
+	"context"
+	"github.com/evryfs/github-actions-runner-operator/api/v1alpha1"
+	"github.com/google/go-github/v31/github"
+	"github.com/gophercloud/gophercloud/testhelper"
+	"github.com/stretchr/testify/mock"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"testing"
+)
+
+func (r *mockApi) GetRunners(organization string, repository string, token string) ([]*github.Runner, error) {
+	args := r.Called(organization, repository, token)
+	return args.Get(0).([]*github.Runner), nil
+}
+
+type mockApi struct {
+	mock.Mock
+}
+
+func TestGithubactionRunnerController(t *testing.T) {
+	const namespace = "someNamespace"
+	const name = "somerunner"
+	const secretName = "someSecretName"
+	const org = "SomeOrg"
+	const repo = ""
+	const token = "someToken"
+	const tokenKey = "GH_TOKEN"
+
+	/*
+		mockResult := []*github.Runner {{
+			ID:     pointer.Int64Ptr(123),
+			Name:   pointer.StringPtr("someName"),
+			OS:     pointer.StringPtr("Linux"),
+			Status: pointer.StringPtr("online"),
+		},
+		}
+	*/
+
+	var mockResult []*github.Runner
+	mockApi := new(mockApi)
+	mockApi.On("GetRunners", org, repo, token).Return(mockResult)
+
+	runner := &v1alpha1.GithubActionRunner{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"label-key": "label-value",
+			},
+		},
+		Spec: v1alpha1.GithubActionRunnerSpec{
+			Organization: org,
+			Repository:   repo,
+			MinRunners:   2,
+			MaxRunners:   2,
+			PodSpec:      v1.PodSpec{},
+			TokenRef: v1.SecretKeySelector{
+				LocalObjectReference: v1.LocalObjectReference{
+					Name: secretName,
+				},
+				Key: tokenKey,
+			},
+		},
+	}
+
+	secret := &v1.Secret{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      secretName,
+		},
+		Data: map[string][]byte{
+			tokenKey: []byte(token),
+		},
+		StringData: nil,
+		Type:       "Opaque",
+	}
+
+	// Objects to track in the fake client.
+	objs := []runtime.Object{runner, secret}
+
+	s := scheme.Scheme
+	s.AddKnownTypes(v1alpha1.SchemeBuilder.GroupVersion, runner)
+
+	cl := fake.NewFakeClientWithScheme(s, objs...)
+
+	r := &GithubActionRunnerReconciler{Client: cl, Log: zap.New(), Scheme: s, githubApi: mockApi}
+
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: namespace,
+			Name:      name,
+		},
+	}
+
+	res, err := r.Reconcile(req)
+	testhelper.AssertNoErr(t, err)
+	testhelper.AssertEquals(t, false, res.Requeue)
+
+	podList := &v1.PodList{}
+	err = r.Client.List(context.TODO(), podList)
+	testhelper.AssertNoErr(t, err)
+	testhelper.AssertEquals(t, runner.Spec.MinRunners, len(podList.Items))
+}
--- go.mod
+++ go.mod
@@ -1,22 +1,17 @@
 module github.com/evryfs/github-actions-runner-operator
 
-go 1.14
+go 1.13
 
 require (
 	github.com/go-logr/logr v0.1.0
 	github.com/google/go-github/v31 v31.0.1-0.20200428005839-378bcfd2d1d7
-	github.com/gophercloud/gophercloud v0.12.0
-	github.com/operator-framework/operator-sdk v0.19.0
-	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.6.1
+	github.com/gophercloud/gophercloud v0.1.0
+	github.com/onsi/ginkgo v1.11.0
+	github.com/onsi/gomega v1.8.1
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
-	k8s.io/api v0.18.6
-	k8s.io/apimachinery v0.18.6
-	k8s.io/client-go v12.0.0+incompatible
-	sigs.k8s.io/controller-runtime v0.6.1
-)
-
-replace (
-	github.com/Azure/go-autorest => github.com/Azure/go-autorest v13.3.2+incompatible // Required by OLM
-	k8s.io/client-go => k8s.io/client-go v0.18.2 // Required by prometheus-operator
+	k8s.io/api v0.18.2
+	k8s.io/apimachinery v0.18.2
+	k8s.io/client-go v0.18.2
+	sigs.k8s.io/controller-runtime v0.6.0
 )
