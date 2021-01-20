c8f495187d0549bfa1af456ea92ca34823c6e6e2
Andrew Block <andy.block@gmail.com>
2021-01-20 00:28:24 -0600

    OLM integration
    Signed-off-by: Andrew Block <andy.block@gmail.com>

--- api/v1alpha1/githubactionrunner_types.go
+++ api/v1alpha1/githubactionrunner_types.go
@@ -2,49 +2,58 @@ package v1alpha1
 
 import (
 	"errors"
+	"time"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"time"
 )
-
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // GithubActionRunnerSpec defines the desired state of GithubActionRunner
 type GithubActionRunnerSpec struct {
 	// Your GitHub organization
 	// +kubebuilder:validation:Required
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Organization",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:text"}
 	Organization string `json:"organization"`
 
 	// Optional Github repository name, if repo scoped.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Repository",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:text"}
 	Repository string `json:"repository,omitempty"`
 
 	// Minimum pool-size. Note that you need one runner in order for jobs to be schedulable, else they fail claiming no runners match the selector labels.
 	// +kubebuilder:validation:Minimum=1
 	// +kubebuilder:validation:Required
 	// +kubebuilder:default=1
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Minimum Pool Size",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:podCount"}
 	MinRunners int `json:"minRunners"`
 
 	// Maximum pool-size. Must be greater or equal to minRunners
 	// +kubebuilder:validation:Minimum=1
 	// +kubebuilder:validation:Required
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Maximum Pool Size",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:podCount"}
 	MaxRunners int `json:"maxRunners"`
 
 	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:XEmbeddedResource
+	// +kubebuilder:pruning:PreserveUnknownFields
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Pod Template"
 	PodTemplateSpec v1.PodTemplateSpec `json:"podTemplateSpec"`
 
 	// PAT to un/register runners. Required if the operator is not running in github-application mode.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Token Reference"
 	TokenRef v1.SecretKeySelector `json:"tokenRef"`
 
 	// How often to reconcile/check the runner pool. If undefined the controller uses a default of 1m
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default="1m"
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Reconciliation Period"
 	ReconciliationPeriod string `json:"reconciliationPeriod"`
 
 	// What order to delete idle pods in
 	// +kubebuilder:default="LeastRecent"
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Deletion Order",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:select:leastrecent","urn:alm:descriptor:com.tectonic.ui:select:mostrecent"}
 	DeletionOrder SortOrder `json:"deletionOrder"`
 }
 
@@ -86,6 +95,7 @@ type GithubActionRunnerStatus struct {
 	// +patchStrategy=merge
 	// +listType=map
 	// +listMapKey=type
+	// +operator-sdk:csv:customresourcedefinitions:type=status,displayName="Conditions",xDescriptors="urn:alm:descriptor:io.kubernetes.conditions"
 	// Details of the current state of this API Resource.
 	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
 }
@@ -95,6 +105,7 @@ type GithubActionRunnerStatus struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=githubactionrunners,scope=Namespaced,shortName=gar
 // +kubebuilder:printcolumn:name="currentPoolSize",type=integer,JSONPath=`.status.currentSize`
+// +operator-sdk:csv:customresourcedefinitions:displayName="GitHub Actions Runner"
 type GithubActionRunner struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
